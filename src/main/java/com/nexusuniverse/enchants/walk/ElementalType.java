9d0e35238f99876739b57a045e087303c66501d6
mrlion101az-cell <mrlion101az@gmail.com>
2026-07-25 00:20:37 -0700

    Delete src/main directory

--- src/main/java/com/nexusuniverse/enchants/walk/ElementalType.java
+++
@@ -1,7 +0,0 @@
-package com.nexusuniverse.enchants.walk;
-
-/** Which liquid a pair of walker boots (or a scroll granting that ability) works on. */
-public enum ElementalType {
-    LAVA,
-    WATER
-}
